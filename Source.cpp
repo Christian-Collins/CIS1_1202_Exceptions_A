d158e481ca14517f06195c252a2756daf89a4b55
Christian Collins <christian.collins1@sinclair.edu>
2024-04-24 20:15:51 -0400

    added source file with two functions, int main and char character. used "#include <iostream>". also added comments with my name, class name, and date I started this project.

---
+++ Source.cpp
@@ -0,0 +1,18 @@
+// Christian Collins
+// CIS 1202 501
+// April 24, 2024
+
+#include <iostream>
+using namespace std;
+
+char character(char start, int offset);
+
+int main()
+{
+	
+}
+
+char character(char start, int offset)
+{
+
+}
