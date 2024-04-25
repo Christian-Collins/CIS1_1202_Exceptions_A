186a708291e26e088d4e89e11cec46a69f91a65f
Christian Collins <christian.collins1@sinclair.edu>
2024-04-25 01:01:29 -0400

    added all statements necessary to check if char start, a variable passed to character function, is a letter, and if int offset, a variable also passed to character function, is low enough that start will still be a character, staying the same case it was originally. It also adds or subtracts offset to/from start. If it stays a letter and stays the same case, it will return start. If its no longer a letter or is a different case, it throws an error. the two errors are invalidCharacterException and invalidRangeException.

--- Source.cpp
+++ Source.cpp
@@ -3,16 +3,66 @@
 // April 24, 2024
 
 #include <iostream>
+#include <cctype>
 using namespace std;
 
 char character(char start, int offset);
 
 int main()
 {
-	
+
 }
 
 char character(char start, int offset)
 {
-
+	string invalidRangeException;
+	if (!(isalpha(start)))
+	{
+		string invalidCharacterException = "";
+		throw invalidCharacterException;
+	}
+	else if (offset > 25 || offset < -25)
+	{
+		throw invalidRangeException;
+	}
+	else if (isupper(start))
+	{
+		start += offset;
+		if (isalpha(start))
+		{
+			if (islower(start))
+			{
+				throw invalidRangeException;
+			}
+			else
+			{
+				return(start);
+			}
+		}
+		else
+		{
+			throw invalidRangeException;
+		}
+	}
+	else if (islower(start))
+	{
+		{
+			start += offset;
+			if (isalpha(start))
+			{
+				if (isupper(start))
+				{
+					throw invalidRangeException;
+				}
+				else
+				{
+					return(start);
+				}
+			}
+			else
+			{
+				throw invalidRangeException;
+			}
+		}
+	}
 }
